40ead8f3ab4048c2bccfac0a2fa5d181e0daa8aa
Christine <you@example.com>
2019-05-17 11:52:02 +0300

    adds get-source article

--- request.py
+++ request.py
@@ -1,4 +1,31 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+
+# Getting the source base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_source(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+
+        source_articles = None
+
+        if get_source_response['articles']:
+            source_articles_list = get_source_response['articles']
+            source_articles = process_articles(source_articles_list)
+
+    return source_articles
