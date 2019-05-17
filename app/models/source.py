437f28b222c192fc29cade72a6414e03d867abf1
Christine <you@example.com>
2019-05-17 11:19:43 +0300

    creates source class

--- app/models/source.py
+++ app/models/source.py
@@ -1,1 +1,9 @@
+class Source:
+     '''
+    Source class to define Source Objects
+    '''
 
+    def __init__(self,id,name,title):
+        self.id =id
+        self.name = name
+        self.title = title
