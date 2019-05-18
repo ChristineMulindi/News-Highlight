55f24e04d0d36b99e577de571bbda4cfcb1a7656
Christine <you@example.com>
2019-05-18 18:02:41 +0300

    modifies source class code

--- app/models/source.py
+++ app/models/source.py
@@ -1,9 +1,11 @@
 class Source:
-     '''
+    '''
     Source class to define Source Objects
     '''
-
-    def __init__(self,id,name,title):
+    def __init__(self,id,name,description,url,category,country):
         self.id =id
-        self.name = name
-        self.title = title
+        self.name =name
+        self.description =description
+        self.url = url
+        self.category =category
+        self.country =country
