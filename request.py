56efb8bfe24b7faf6cea27bf7df602648caf26d8
Christine <you@example.com>
2019-05-18 17:57:39 +0300

    creates code to make requests to API

--- request.py
+++ request.py
@@ -1,51 +1,61 @@
 from app import app
 import urllib.request,json
-from .models import source
+from app.models import source
 
 Source = source.Source
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 
 # Getting the source base url
-base_url = app.config["NEWS_API_BASE_URL"]
+base_url = app.config['SOURCE_API_BASE_URL']
 
 
-def get_source(category):
+def get_sources(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_source_url = base_url.format(category, api_key)
+    get_sources_url = base_url.format(category, api_key)
+    print(get_sources_url)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
-    with urllib.request.urlopen(get_source_url) as url:
-        get_source_data = url.read()
-        get_source_response = json.loads(get_source_data)
+        sources_results = None
 
-        source_articles = None
+        if get_sources_response['articles']:
+            sources_results_list = get_sources_response['articles']
+         
+            sources_results = process_results(sources_results_list)
+            
 
-        if get_source_response['articles']:
-            source_articles_list = get_source_response['articles']
-            source_articles = process_articles(source_articles_list)
+    return sources_results
+   
 
-    return source_articles
-
-
-def process_articles(source_list):
+def process_results(source_list):
     '''
-    Function  that processes the source article and transform them to a list of Objects
+    Function  that processes the source result and transform them to a list of Objects
 
     Args:
         source_list: A list of dictionaries that contain source details
 
     Returns :
-        source_articles: A list of source objects
+        sources_results: A list of source objects
     '''
-    source_articles = []
+    sources_results = []
+
     for source_item in source_list:
         id = source_item.get('id')
         name = source_item.get('name')
-        title = source_item.get('title')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        country = source_item.get('country')
+        
+        source_object = Source(id,name,description,url,category,country )
+        sources_results.append(source_object)
+    return sources_results
+    print(sources_results)
     
-
-    return source_articles
