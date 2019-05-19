1497dbed2ff85e3683100dfeba2b6926595f5d74
Christine <you@example.com>
2019-05-20 00:52:03 +0300

    moves articles and source test files to test folder

--- app/tests/source_test.py
+++ app/tests/source_test.py
@@ -12,7 +12,8 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('cnn', 'CNN', 'In Tehran, specter of war met with more defiance than fear - CNN')
+        self.new_source = Source(
+            'cnn', 'CNN', 'In Tehran, specter of war met with more defiance than fear - CNN', 'https://abcnews.go.com', 'general', 'no')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source, Source))
